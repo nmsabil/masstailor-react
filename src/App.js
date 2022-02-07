3c054e0b9428c5ce154b6d95f10c2f1d03bc6fb8
Sabil <sabilmdtv1@gmail.com>
2022-02-07 22:10:11 +0000

    added bootstrap and test navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import "./App.scss";
 
+import Navigation from "./components/Navigation/Navigation";
+
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation></Navigation>
     </div>
   );
 }
