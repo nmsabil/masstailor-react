9c6e6e628dd5118f79edbe4d4c222f966232ac68
Sabil <sabilmdtv1@gmail.com>
2022-02-10 21:08:14 +0000

    added navigation boiler plate and container class

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,33 @@
 import React from "react";
+import logo from "../../images/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhoneFlip } from "@fortawesome/free-solid-svg-icons";
 
 function Navigation() {
-  return <h1>hello</h1>;
+  return (
+    <div className='navbar'>
+      <div className='container'>
+        <div className='navbar_logo'>
+          <img src={logo} alt='' />
+        </div>
+        <div className='navbar_links'>
+          <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>About</li>
+            <li>Work</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className='navbar_cta'>
+          <div>
+            <FontAwesomeIcon className='me-2' icon={faPhoneFlip} />
+            <span>07424605611</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navigation;
