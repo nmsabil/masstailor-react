9f946ff407f3f280fc061c8d7f3b664d36256e34
Sabil <sabilmdtv1@gmail.com>
2022-02-10 20:46:52 +0000

    removed bootstrap and created folders for each component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import Navigation from "./components/Navigation/Navigation";
+import Hero from "./components/Hero/Hero";
+import Services from "./components/Services/Services";
+import Reviews from "./components/Reviews/Reviews";
+import About from "./components/About/About";
+import Work from "./components/Work/Work";
+import Contact from "./components/Contact/Contact";
+import Map from "./components/Map/Map";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className='App'>
-      <Navigation></Navigation>
+      <Navigation />
+      <Hero />
+      <Services />
+      <Reviews />
+      <About />
+      <Work />
+      <Contact />
+      <Map />
+      <Footer />
     </div>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,36 +1,7 @@
 import React from "react";
-import { Container, Nav, Navbar, Image } from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPhoneFlip } from "@fortawesome/free-solid-svg-icons";
 
-import logo from "../../images/logo.png";
 function Navigation() {
-  return (
-    <Navbar id='mainNav' bg='light' variant='dark' expand='lg'>
-      <Container>
-        <Navbar.Brand href='#home'>
-          <Image style={{ width: 150 }} src={logo}></Image>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse className='justify-content-end' id='basic-navbar-nav'>
-          <Nav className='text-center align-items-center'>
-            <Nav.Link href='#home'>Home</Nav.Link>
-            <Nav.Link href='#link'>Services</Nav.Link>
-            <Nav.Link href='#link'>About</Nav.Link>
-            <Nav.Link href='#link'>Our Work</Nav.Link>
-            <Nav.Link href='#link'>Contact</Nav.Link>
-            <Nav.Link
-              className='phone mt-2 mb-5 mt-lg-auto mb-lg-auto d-flex align-items-center justify-content-center ms-lg-5 ms-md-0 px-3 px-lg-2 rounded-pill'
-              href='#link'
-            >
-              <FontAwesomeIcon className='me-2' icon={faPhoneFlip} />
-              07424605611
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+  return <h1>hello</h1>;
 }
 
 export default Navigation;
