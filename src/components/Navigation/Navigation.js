89b3c779756ae29209f75603bfcef87044bff009
Sabil <sabilmdtv1@gmail.com>
2022-02-14 21:53:27 +0000

    desktop navbar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,18 +1,51 @@
 import React from "react";
-import logo from "../../images/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhoneFlip } from "@fortawesome/free-solid-svg-icons";
 
 function Navigation() {
   return (
     <div className='navbar'>
-      <div className='container navbar_container'>
-        <div className='navbar_logo'>
-          <a href='/'>
-            <img src={logo} alt='MasssTailor Logo' />
+      <div className='desktop'>
+        <div className='container desktop_container'>
+          <div className='desktop_logo'>
+            <a href='/'>
+              MASSS <span>TAILOR</span>
+            </a>
+          </div>
+          <div className='desktop_links'>
+            <ul>
+              <li>
+                <a href='/'>Home</a>
+              </li>
+              <li>
+                <a href='/'>Services</a>
+              </li>
+              <li>
+                <a href='/'>About</a>
+              </li>
+              <li>
+                <a href='/'>Work</a>
+              </li>
+              <li>
+                <a href='/'>Contact</a>
+              </li>
+            </ul>
+          </div>
+          <div className='desktop_cta'>
+            <div>
+              <a href='/'>
+                <FontAwesomeIcon icon={faPhoneFlip} />
+                <span>07424605611</span>
+              </a>
+            </div>
+          </div>
+          <a className='mobile-nav-button'>
+            <span></span>
           </a>
         </div>
-        <div className='navbar_links'>
+      </div>
+      <div className='navbar mobile'>
+        <nav>
           <ul>
             <li>
               <a href='/'>Home</a>
@@ -30,15 +63,7 @@ function Navigation() {
               <a href='/'>Contact</a>
             </li>
           </ul>
-        </div>
-        <div className='navbar_cta'>
-          <div>
-            <a href='/'>
-              <FontAwesomeIcon icon={faPhoneFlip} />
-              <span>07424605611</span>
-            </a>
-          </div>
-        </div>
+        </nav>
       </div>
     </div>
   );
