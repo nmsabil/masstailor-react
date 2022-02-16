3c3cb385c054f323dfaf70ac3f16d5d3c67203db
Sabil <sabilmdtv1@gmail.com>
2022-02-16 21:47:10 +0000

    created layout for hero and installed slick slider

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,36 @@
 import React from "react";
+import Slider from "react-slick";
+import img1 from "../../images/hero/IMG_8572.jpg";
+import img2 from "../../images/hero/IMG_8626.jpg";
+import img3 from "../../images/hero/IMG_8646.jpg";
+import img4 from "../../images/hero/IMG_8672.jpg";
 
 function Hero() {
-  return <div>Hero</div>;
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <div className='hero-container'>
+      <Slider {...settings}>
+        <div className='slide'>
+          <img src={img1} alt='' />
+        </div>
+        <div className='slide'>
+          <img src={img2} alt='' />
+        </div>
+        <div className='slide'>
+          <img src={img3} alt='' />
+        </div>
+        <div className='slide'>
+          <img src={img4} alt='' />
+        </div>
+      </Slider>
+    </div>
+  );
 }
 
 export default Hero;
