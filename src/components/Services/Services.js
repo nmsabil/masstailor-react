8f3a5ca3c68a90638a932fd202bf1dbbc8226358
Sabil <sabilmdtv1@gmail.com>
2022-02-21 21:11:09 +0000

    created text anomation and service section

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 function Services() {
-  return <div>Services</div>;
+  return (
+    <div className='services'>
+      <div className='container'>
+        <div class='patterns'>
+          <svg width='100%' height='100%'>
+            <text x='50%' y='50%' text-anchor='middle'>
+              Services
+            </text>
+          </svg>
+        </div>
+        <h3>We are here for you</h3>
+        <p>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est
+          laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+          do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Services;
