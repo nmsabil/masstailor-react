a9a89ba7b8b09044089b490d3e680495da667197
Sabil <sabilmdtv1@gmail.com>
2022-02-24 21:16:10 +0000

    made the service section responsive and added content

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -21,17 +21,47 @@ function Services() {
         <h3>
           We are here <span>for you</span>
         </h3>
-        <p>
+        <p className='subtitle'>
           If you are looking for exceptional tailoring service in London, then
           look no further!
         </p>
         <div className='service-card'>
-          <Card src={img1} alt='a' />
-          <Card src={img2} alt='b' />
-          <Card src={img3} alt='c' />
-          <Card src={img4} alt='d' />
-          <Card src={img5} alt='e' />
-          <Card src={img6} alt='f' />
+          <Card
+            src={img1}
+            alt='IndianDress'
+            title='Indian Dress'
+            description='Our made to measure Indian Dress service starts with a personalised one to one meeting at our premises. All Indian suits are hand drafted, cut and fitted to the shape and style of our customers.'
+          />
+          <Card
+            src={img2}
+            alt='PunjabiDress'
+            title='Punjabi Dress'
+            description='Punjabi Suits are personally hand drafted, cut and fitted to the shape and style of our customers. Our handmade bespoke suit service is known to be on par with the best bespoke tailors in the world.'
+          />
+          <Card
+            src={img3}
+            alt='SareeBlouse'
+            title='Saree Blouse'
+            description='For all kinds of dress, western and Indian, we provide alterations. Designs to match and fit, different necklines and sleeves to choose.'
+          />
+          <Card
+            src={img4}
+            alt='SareeFall'
+            title='Saree Falls'
+            description='Variety of professionally stitched saree falls. Exotic patterns and borders accents the elegance of your sarees.'
+          />
+          <Card
+            src={img5}
+            alt='Suit'
+            title='Suit'
+            description='Both Full & Semi Bespoke items are hand drafted, cut, and stitched.'
+          />
+          <Card
+            src={img6}
+            alt='Alteration'
+            title='Alteration'
+            description='Alterations to pants and jeans. Shorten or lengthen, take in or let out. Personalise your outfit'
+          />
         </div>
       </div>
     </div>
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -6,14 +6,8 @@ function Card(props) {
       <figure class='card__thumb'>
         <img src={props.src} alt={props.alt} class='card__image' />
         <figcaption class='card__caption'>
-          <h2 class='card__title'>
-            NASA Has Found Hundreds Of Potential New Planets
-          </h2>
-          <p class='card__snippet'>
-            NASA released a list of 219 new “planet candidates” discovered by
-            the Kepler space telescope, 10 of which are similar to Earth’s size
-            and may be habitable by other life forms.
-          </p>
+          <h2 class='card__title'>{props.title}</h2>
+          <p class='card__snippet'>{props.description}</p>
           <a href='/' class='card__button'>
             Read more
           </a>
