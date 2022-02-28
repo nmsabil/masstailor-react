61631fca3546b80881475067576c14aa2aec21be
Sabil <sabilmdtv1@gmail.com>
2022-02-28 21:35:33 +0000

    added reviews boiler plate

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 function Reviews() {
-  return <div>Reviews</div>;
+  return (
+    <div className='services'>
+      <div className='container'>
+        <div class='patterns'>
+          <svg width='100%' height='100%'>
+            <text x='50%' y='50%' text-anchor='middle'>
+              Reviews
+            </text>
+          </svg>
+        </div>
+        <h3>
+          What Our <span>customers say</span>
+        </h3>
+        <p className='subtitle'>
+          If you are looking for exceptional tailoring service in London, then
+          look no further!
+        </p>
+        <div className='service-card'></div>
+      </div>
+    </div>
+  );
 }
 
 export default Reviews;
