88fb2b968243858b3925b0df7da55d8114f93472
Sabil <sabilmdtv1@gmail.com>
2022-03-14 20:19:21 +0000

    about header added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div className='about'>
+      <div className='container'>
+        <div className='header'>
+          <div className='patterns'>
+            <svg width='100%' height='100%'>
+              <text x='50%' y='50%' textAnchor='middle'>
+                About
+              </text>
+            </svg>
+          </div>
+          <h3>
+            What Our <span>customers say</span>
+          </h3>
+        </div>
+        <div className='review'></div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
