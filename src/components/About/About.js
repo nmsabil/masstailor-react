281db41c254d357052a3c5196068742574cfd6bd
Sabil <sabilmdtv1@gmail.com>
2022-03-15 20:52:36 +0000

    created about us section, added image and content.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,24 +1,36 @@
 import React from "react";
+import img from "../../images/hero/IMG_8572.jpg";
 
 function About() {
   return (
     <div className='about'>
-      <div className='container'>
-        <div className='header'>
-          <div className='patterns'>
-            <svg width='100%' height='100%'>
-              <text x='50%' y='50%' textAnchor='middle'>
-                About
-              </text>
-            </svg>
-          </div>
-          <h3>
-            What Our <span>customers say</span>
-          </h3>
+      <div className='header'>
+        <div className='patterns'>
+          <svg width='100%' height='100%'>
+            <text x='50%' y='50%' textAnchor='middle'>
+              About
+            </text>
+          </svg>
         </div>
-        <div className='about-content'>
-          <div className='image'>b</div>
-          <div className='content'>a</div>
+        <h3>
+          <span>Background</span>
+        </h3>
+      </div>
+      <div className='about-content'>
+        <img src={img} className='image' />
+        <div className='content'>
+          <div>
+            Marajuddin grew up with a tape measure draped around his neck. His
+            father drew him to his side as a young child so that he could have
+            an unimpeded view of what he did—and what his father before him had
+            done—for a living. What remains with him, foremost, from those
+            earliest, foreign moments was his father’s demeanor: gracious,
+            patient, focused.
+            <span>
+              Mr Maraj is Asian womenswear specialist located in Southall,
+              London
+            </span>
+          </div>
         </div>
       </div>
     </div>
