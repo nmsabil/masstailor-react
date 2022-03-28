393f805113180072fb1d23a448dbe8339e6f591f
Sabil <sabilmdtv1@gmail.com>
2022-03-28 21:26:19 +0100

    added form boilerplate

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,26 @@
 import React from "react";
+import ContactForm from "./ContactForm";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className='contact'>
+      <div className='header'>
+        <div className='patterns'>
+          <svg width='100%' height='100%'>
+            <text x='50%' y='50%' textAnchor='middle'>
+              Contact
+            </text>
+          </svg>
+        </div>
+        <h3>
+          Get in<span> touch</span>
+        </h3>
+      </div>
+      <div className='gallery'>
+        <ContactForm />
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
